06cbaa8ae23ec06e60b0594e1ca00951e4f75b53
Aubrey Crook <aubreycrook@hotmail.com>
2022-01-22 19:12:02 -0500

    Created routes, app is getting and saving from the database

--- server.js
+++ server.js
@@ -1,7 +1,46 @@
 const express = require("express");
+const { sendFile, json } = require("express/lib/response");
+const path = require("path");
+const fs = require("fs");
+const { resolve } = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
+app.use(express.json());
 
+app.get("/notes",(req,res)=>{
+    res.sendFile("./public/notes.html",{
+        root: path.resolve(__dirname,".")
+    })
+})
+
+
+app.post("/api/notes",(req,res)=>{
+    let data = fs.readFileSync(path.resolve(__dirname,"db/db.json")).toString()
+    let saveNotes = JSON.parse(data)
+    saveNotes.push(req.body)
+    let datatoSave = JSON.stringify(saveNotes)
+    fs.writeFileSync(path.resolve(__dirname,"db/db.json"),datatoSave)
+
+   console.log("saved notes")
+})
+
+
+app.delete("/api/notes",(req,res)=>{
+    let data = fs.readFileSync(path.resolve(__dirname,"db/db.json")).toString()
+    let saveNotes = JSON.parse(data)
+    saveNotes.push(req.body)
+    let datatoSave = JSON.stringify(saveNotes)
+    fs.writeFileSync(path.resolve(__dirname,"db/db.json"),datatoSave)
+
+   console.log("saved notes")
+})
+
+app.get("/api/notes",(req,res)=>{
+   res.sendFile(path.resolve(__dirname,"db/db.json"))
+})
+//app.get("*",(req,res)=>{
+    
+//})
 
 app.use(express.static("public"));
 app.listen(port, OnListening);
