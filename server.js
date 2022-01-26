6ce83b4d0a5bd8ee92c89d5c4318d6df943dee37
Aubrey Crook <aubreycrook@hotmail.com>
2022-01-26 18:04:09 -0500

    made changes to delete notes and refreshes notes

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const { sendFile, json } = require("express/lib/response");
 const path = require("path");
-const fs = require("fs");
+const morgan = require ("morgan");
+const fs = require("fs");;
 const { resolve } = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
+app.use(morgan('tiny'))
 
 app.get("/notes",(req,res)=>{
     res.sendFile("./public/notes.html",{
@@ -17,24 +19,33 @@ app.get("/notes",(req,res)=>{
 app.post("/api/notes",(req,res)=>{
     let data = fs.readFileSync(path.resolve(__dirname,"db/db.json")).toString()
     let saveNotes = JSON.parse(data)
-    saveNotes.push(req.body)
+    let notetoSave = {
+        id: saveNotes.length, 
+        title: req.body.title, 
+        text: req.body.text
+    }
+    saveNotes.push(notetoSave)
     let datatoSave = JSON.stringify(saveNotes)
+    console.log(datatoSave)
     fs.writeFileSync(path.resolve(__dirname,"db/db.json"),datatoSave)
+    res.sendStatus(200)
 
    console.log("saved notes")
 })
 
 
-app.delete("/api/notes",(req,res)=>{
+app.delete("/api/notes/:id",(req,res)=>{
     let data = fs.readFileSync(path.resolve(__dirname,"db/db.json")).toString()
     let saveNotes = JSON.parse(data)
-        saveNotes.filter(note => {
-        return note.title != req.body.title;
+        saveNotes = saveNotes.filter(note => {
+            console.log(note.id)
+            console.log(req.params)
+        return note.id != parseInt(req.params.id);
     })
     console.log(req.body)
     let datatoSave = JSON.stringify(saveNotes)
     fs.writeFileSync(path.resolve(__dirname,"db/db.json"),datatoSave)
-
+res.sendStatus(200)
    console.log("saved notes")
 })
 
